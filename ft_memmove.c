c87d87963856277e66e5c91dd3a3732b5741fd8d
perso <perso@MacBook-Air-de-perso-2.local>
2021-01-12 07:37:50 -0800

    ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ppitavy <ppitavy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 02:46:06 by ppitavy           #+#    #+#             */
-/*   Updated: 2021/01/12 07:29:25 by ppitavy          ###   ########.fr       */
+/*   Updated: 2021/01/12 07:37:27 by ppitavy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t	i;
-	char	*cpy;
+	unsigned char	*cpy;
 
 	if (!dest && !src)
 		return (0);
 	i = 0;
-	cpy = (char *)dest;
+	cpy = (unsigned char *)dest;
 	if (dest > src)
 	{
 		n--;
 		while (n > 0)
 		{
-			cpy[n] = ((char *)src)[n];
+			cpy[n] = ((unsigned char *)src)[n];
 			n--;
 		}
 	}
@@ -34,7 +34,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	{
 		while (i < n)
 		{
-			cpy[i] = ((char *)src)[i];
+			cpy[i] = ((unsigned char *)src)[i];
 			i++;
 		}
 	}
