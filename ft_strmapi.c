c0bb5c206bddf8c2819fc22e524ae4aa0e4c7571
perso <perso@MacBook-Air-de-perso-2.local>
2021-01-13 07:36:25 -0800

    ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppitavy <ppitavy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/13 07:28:17 by ppitavy           #+#    #+#             */
+/*   Updated: 2021/01/13 07:35:49 by ppitavy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (!(str = malloc(sizeof(char) * (ft_strlen(s) + 1))))
+		return (0);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
